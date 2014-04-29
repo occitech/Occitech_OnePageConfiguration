ec92820a541cea99d6677676663339245cc638d7
Damien Biasotto <damien@occi-tech.com>
2014-04-29 17:27:52 +0200

    Proof of concept.

---
+++ OnePageConfiguration/Helper/Data.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: damien
+ * Date: 29/04/14
+ * Time: 16:49
+ */ 
+class Occitech_OnePageConfiguration_Helper_Data extends Mage_Core_Helper_Abstract {
+
+}
