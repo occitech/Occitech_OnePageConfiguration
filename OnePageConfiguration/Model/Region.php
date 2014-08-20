ef24d97324ad68abe3327bbfe05261e8f4aacdd8
Adrien Louis-Rossignol <adrien@occitech.fr>
2014-08-20 17:53:48 +0200

    Can now set regions asking for France

--- OnePageConfiguration/Model/Region.php
+++ OnePageConfiguration/Model/Region.php
@@ -2,10 +2,20 @@
 
 class Occitech_OnePageConfiguration_Model_Region extends Mage_Core_Model_Config_Data {
 	public function save() {
-		$resource = Mage::getModel('core/config');
-		$stateRequired = Mage::getStoreConfig('general/region/state_required');
-		$resource->saveConfig('general/region/state_required', 'DE,AT,CA,ES,EE,FI,LV,LT,RO,CH,US');
-		$resource->saveConfig('general/region/display_all', 0); // prevent displaying regions if they are not required
+		if ($this->isValueChanged()) {
+			$resource = Mage::getModel('core/config');
+			$stateRequired = Mage::getStoreConfig('general/region/state_required');
+			$displayRegion = $this->getValue();
+			if ($displayRegion) {
+				if (strpos($stateRequired, ',FR') === false) {
+					$stateRequired = $stateRequired . ',FR';
+				}
+			} else {
+				$stateRequired = str_replace(',FR', '', $stateRequired);
+			}
+			$resource->saveConfig('general/region/state_required', $stateRequired);
+			$resource->saveConfig('general/region/display_all', 0); // prevent displaying regions if they are not required
+		}
 		return parent::save();
 	}
 }
